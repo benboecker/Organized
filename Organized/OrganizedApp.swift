790eb779150de7d0917c9a02afc4f61d0e0878db
Ben Boecker <benjamin.boecker@palasthotel.de>
2024-04-19 17:14:08 +0200

    initial project

---
+++ Organized/OrganizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizedApp.swift
+//  Organized
+//
+//  Created by Benjamin Böcker on 19.04.24.
+//
+
+import SwiftUI
+
+@main
+struct OrganizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
